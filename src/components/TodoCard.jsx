675e73fedc8e472154c2b5f1742237cb4d148814
S M Tareq Aziz <smtareqaziz007@gmail.com>
2024-03-24 01:26:41 +0600

    Delete Confirmation Added

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { Card, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Button, Modal } from "react-bootstrap";
 import { BsTrash } from 'react-icons/bs';
 import { TodoForm } from "./TodoForm";
 
 const TodoCard = ({ note, updateNote, deleteNote }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleDelete = () => {
+    deleteNote(note.id);
+    setShowModal(false); // Close the modal after deletion
+  };
+
   return (
+    <>
     <Card className="shadow-sm rounded">
       <Card.Body>
         <Card.Title className="fw-bold">{note.title}</Card.Title>
@@ -16,11 +24,25 @@ const TodoCard = ({ note, updateNote, deleteNote }) => {
           </div>
           <div>
             <TodoForm submitNote={updateNote} defaultNote={note} label={"Update"} />
-            <Button className="btn btn-danger ms-2" onClick={() => deleteNote(note.id)}><BsTrash/></Button>
+            <Button className="btn btn-danger ms-2" onClick={() => setShowModal(true)}><BsTrash/></Button>
           </div>
         </div>
       </Card.Body>
     </Card>
+
+    { showModal &&
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Deletion</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShowModal(false)}>Cancel</Button>
+        <Button variant="danger" onClick={handleDelete}>Delete</Button>
+      </Modal.Footer>
+     </Modal>
+    }
+    </>
   );
 };
 
